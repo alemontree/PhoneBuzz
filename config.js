9161e05b2a7aae5814bdd4d18358b915139f73d8
Mikhail <mishnade@gmail.com>
2015-09-11 20:37:32 -0700

    change config

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ module.exports = {
     // A Twilio number that you have purchased through the twilio.com web
     // interface or API
     // twilioNumber: "9083585201",
-    twilioNumber: "+19082646579",
-    // twilioNumber: "+19083585201",
+    // twilioNumber: "+19082646579",
+    twilioNumber: "+19083585201",
 
     // The port your web application will run on
     port: process.env.PORT || 8000
